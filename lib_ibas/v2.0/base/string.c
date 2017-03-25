1c75e855bf9fc5aa544072ca46bb1cdb0090f2e0
jgrdlgrd <jgrdlgrd@gmail.com>
2017-03-25 22:53:58 +0400

    Preparing to dispose of genericList. Removed it from String.

--- lib_ibas/v2.0/base/string.c
+++ lib_ibas/v2.0/base/string.c
@@ -4,15 +4,50 @@
 
 #include "string.h"
 
-String_t __String_t_create(size_t size) {
+String_t __String_create(size_t size) {
   return Vector.create(size, sizeof(char));
 }
 
-//CLion shows an error here, but actually there's no error TODO suppress the error
-genericListMethodsImplStripped(Vector, String_t, str, char)
+void __String_destroy(String_t str) {
+  Vector.destroy(str);
+}
 
-String_t __String_t_toString(String_t str) {
-  return str;
+char __String_get(String_t str, int i) {
+  return *(char*)Vector.get(str, i);
+}
+
+void __String_set(String_t str, int i, char val) {
+  Vector.set(str, i, &val);
+}
+
+void __String_add(String_t str, char val) {
+  Vector.add(str, &val);
+}
+
+void __String_insert(String_t str, int i, char val) {
+  Vector.insert(str, i, &val);
+}
+
+void __String_remove(String_t str, int i) {
+  Vector.remove(str, i);
+}
+
+bool __String_forEachIntermediate(String_t str, Object element, Object ctx) {
+  Pair *p = (Pair*) ctx;
+  return ((bool(*)(String_t, char, Object))(p->first))(str, *(char*)element, p->second);
+}
+
+void __String_forEach(String_t str, bool (*func)(String_t str, char element, Object ctx), Object ctx) {
+  Pair p = {func, ctx};
+  Vector.forEach(str, __String_forEachIntermediate, &p);
+}
+
+int __String_find(String_t str, char val) {
+  return Vector.find(str, &val);
+}
+
+void __String_clear(String_t str) {
+  Vector.clear(str);
 }
 
 //TODO implement
@@ -29,8 +64,17 @@ void __String_prependCStr(String_t str2, CString str1) {
 }
 
 String_t_ String = {
-  genericListMethodsList(String_t),
-  __String_concat,
-  __String_appendCStr,
-  __String_prependCStr
+    __String_create,
+    __String_destroy,
+    __String_get,
+    __String_set,
+    __String_add,
+    __String_insert,
+    __String_remove,
+    __String_forEach,
+    __String_find,
+    __String_clear,
+    __String_concat,
+    __String_appendCStr,
+    __String_prependCStr
 };
--- lib_ibas/v2.0/base/string.h
+++ lib_ibas/v2.0/base/string.h
@@ -4,19 +4,28 @@
 
 #pragma once
 
-#include "lib_ibas/v2.0/lib_ibas.h"
-#include "lib_ibas/v2.0/containers/vector.h"
-#include "lib_ibas/v2.0/containers/generic-list.h"
+#include "../lib_ibas.h"
+#include "../containers/vector.h"
 
 typedef Vector_s String_s;
 typedef Vector_t String_t;
 
 declareClass(String, {
   String_t (*create)(size_t size);
+  void (*destroy)(String_t str);
 
-  genericListMethodsDeclStripped(String_t, str, char);
+  char (*get)(String_t str, int i);
+  void (*set)(String_t str, int i, char val);
 
-  String_t (*toString)(String_t str);
+  void (*add)(String_t str, char val);
+  void (*insert)(String_t str, int i, char val);
+  void (*remove)(String_t str, int i);
+
+  void (*forEach)(String_t str, bool (*func)(String_t str, char ch, Object ctx), Object ctx);
+  int (*find)(String_t str, char val);
+
+  void (*clear)(String_t str);
+
   void (*concat)(String_t str1, String_t str2);
   void (*appendCStr)(String_t str1, CString str2);
   void (*prependCStr)(String_t str2, CString str1);
